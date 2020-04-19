a1fd82cb231bb0af63c4cdd538f0027de52d4450
ruth <ruthwanjiramugo@gmail.com>
2020-04-19 23:45:58 +0300

    add the else if function for the login

--- run.py
+++ run.py
@@ -55,78 +55,78 @@ def find_first_name(credential):
     return Credential.find_by_first_name(credential)
 
 
-# def check_existing_credentials(credential):
-#     """Another exciting class gives users the privilege to check whether certain accounts, names or other details
-#     exist """
-#     return Credential.credential_exist(credential)
+def check_existing_credentials(credential):
+    """Another exciting class gives users the privilege to check whether certain accounts, names or other details
+    exist """
+    return Credential.credential_exist(credential)
 
 
-# def display_credentials():
-#     """Depending on existing credentials, a user can utilize this feature because it does display the number of
-#     credentials available """
-#     return Credential.display_credentials()
+def display_credentials():
+    """Depending on existing credentials, a user can utilize this feature because it does display the number of
+    credentials available """
+    return Credential.display_credentials()
 
 
-# def display_user():
-#     """This class has the power to display users credentials"""
-#     return User.display_users()
+def display_user():
+    """This class has the power to display users credentials"""
+    return User.display_users()
 
 
-# def main():
-#     print("Welcome to Locked-In. From the list of predetermined commands, choose one or more")
-#     while True:
-#         print("Basic Commands: \n can - create a new_user account with a user_defined password\n cag- create a new "
-#               "auto-generated password\n  lg - log into your Password Locker account \n cad - display all user "
-#               "accounts \n ex -exit the contact "
-#               "list \n")
-#         short_code = input().lower()
+def main():
+    print("Welcome to Locked-In. From the list of predetermined commands, choose one or more")
+    while True:
+        print("Basic Commands: \n can - create a new_user account with a user_defined password\n cag- create a new "
+              "auto-generated password\n  lg - log into your Password Locker account \n cad - display all user "
+              "accounts \n ex -exit the contact "
+              "list \n")
+        short_code = input().lower()
 
-#         if short_code == 'can':
-#             print("New user")
-#             print("-" * 10)
-#             print(" Hello! What account would you like to create credentials for? ")
-#             site = input()
-#             print("->->->->")
+        if short_code == 'can':
+            print("New user")
+            print("-" * 10)
+            print(" Hello! What account would you like to create credentials for? ")
+            site = input()
+            print("->->->->")
 
-#             print("First name ....")
-#             f_name = input()
+            print("First name ....")
+            f_name = input()
 
-#             print("Last name")
-#             l_name = input()
+            print("Last name")
+            l_name = input()
 
 
-#             print("Email address")
-#             e_address = input()
+            print("Email address")
+            e_address = input()
 
-#             print("Enter username ...")
-#             user_name = input()
+            print("Enter username ...")
+            user_name = input()
 
-#             print("Enter password")
-#             password = input()
+            print("Enter password")
+            password = input()
 
-#             save_user(create_user(f_name, l_name,e_address,password))
-#             save_credentials(create_credential(f_name, l_name, password))
-#             print("\n")
-#             print(f"A new {site} account by {f_name} successfully has been created")
-#             print(f"The user-name is {user_name} and the password is {password} ")
-#             print("\n")
+            save_user(create_user(f_name, l_name,e_address,password))
+            save_credentials(create_credential(f_name, l_name, password))
+            print("\n")
+            print(f"A new {site} account by {f_name} successfully has been created")
+            print(f"The user-name is {user_name} and the password is {password} ")
+            print("\n")
 
-#         elif short_code == 'lg':
-#             '''
-#             Logs in the user into their Password Locker account
-#             '''
-#             print("\n")
-#             print("Log into Password Locker Account")
-#             print("Enter the user name")
-#             user_name = input()
+        elif short_code == 'lg':
+            '''
+            Logs in the user into their Password Locker account
+            '''
+            print("\n")
+            print("Log into Password Locker Account")
+            print("Enter the user name")
+            user_name = input()
 
-#             print("Enter the password")
-#             user_password = input()
+            print("Enter the password")
+            user_password = input()
 
-#             if user_log_in(user_name, user_password) is None:
-#                 print("\n")
-#                 print("Please try again or create an account")
-#                 print("\n")
+            if user_log_in(user_name, user_password) is None:
+                print("\n")
+                print("Please try again or create an account")
+                print("\n")
 
 #         elif short_code == "cag":
 #             print("New user")
